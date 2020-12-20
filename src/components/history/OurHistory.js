972687ea58b531874de16e681005630224c082fe
Sergei Kompanietc <delightvlg@gmail.com>
2020-12-20 21:26:15 +0300

    Released routing

---
+++ src/components/history/OurHistory.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './OurHistory.css';
+
+function OurHistory() {
+  return (
+    <div className="history-temporary ">OUR HISTORY</div>
+  )
+}
+
+export default OurHistory;
